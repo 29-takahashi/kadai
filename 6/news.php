b780d949377ff39ebc9e3a1dd78df8c5801f592e
29-takahashi <pero@dream.jp>
2015-09-06 17:09:19 +0900

    ヘッダー、フッター化を実装。

---
+++ 6/news.php
@@ -0,0 +1,17 @@
+<?php include("header.php"); ?>
+    
+<section class="news contents-box">
+    <h2 class="section-title text-center">
+        <span class="section-title__yellow">News</span>
+        <span class="section-title-ja text-center">日付</span>
+    </h2>
+    <article class="news-detail">
+        <dl class="clearfix">
+            <dd class="news-title">ニュースタイトル</dd>
+            <dd>ニュース詳細：あいうえおかきくけこさしすせそたちつてと</dd>
+        </dl>
+        
+    </article>
+</section>
+
+<?php include("footer.php"); ?>
