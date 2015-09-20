eebc51505462ebfaacc96bff80ce375bbdd5494c
29-takahashi <pero@dream.jp>
2015-09-20 10:34:56 +0900

    ログイン画面を整え。

--- 6/admin/login.php
+++ 6/admin/login.php
@@ -1,10 +1,11 @@
 <?php include("header.php"); ?>
 
-<h1>ログイン</h1>
-<form action="login_execute.php" method="post">
-	<p>ID: <input type="text" name="id" value="" />
-	Password: <input type="password" name="pw" value="" />
-	<input type="submit" value="ログイン" class="btn btn-primary btn-xs" /></p>
-</form>
+<div class="well well-lg text-center">
+	<form action="login_execute.php" method="post">
+		<p><input type="text" name="id" placeholder="ID" /></p>
+		<p><input type="password" name="pw" placeholder="Password" /></p>
+		<p><input type="submit" value="ログイン" class="btn btn-success" /></p>
+	</form>
+</div>
 
 <?php include("footer.php"); ?>
